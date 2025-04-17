36a2e113adff528d68b50b966bde376b69a67ca5
tutur <B00813338@essec.edu>
2025-04-17 19:16:44 +0200

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/axel-crm/',  // <-- clé pour GitHub Pages
   plugins: [react()],
 })
